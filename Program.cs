a930aed673e7884fde9661e86bb19f31eed98f01
AhmedShaker940 <a7medsar7an123@gmail.com>
2026-09-16 05:34:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C48_G03_EXAM01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
